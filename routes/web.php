0b1b0327bcafc506e7b5474166345800f354606b
Yusuf Abid <yusufabid.m@gmail.com>
2023-02-04 09:53:32 +0700

    adding tailwindcss and creating index page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// ------- Admin Routes -------
+
+
+// public routes
+Route::get('admin/login', [AdminController::class, 'login']);
+
+// POST
+Route::post('admin/login', [AdminController::class, 'login']);
+
+// private routes
+Route::prefix('admin')->middleware('adminAuth')->group(function () {
+
 });
